30b354d08728656db39b25822a4946ff19273be5
Andrey Kovalev <akovalevhse@gmail.com>
2022-09-18 16:24:41 +0300

    minor code fixes

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,32 +10,62 @@ namespace Methods
     {
         static string[] StringToArray(string sentence)
         {
-            int wordsCounter = 1; //количество слов в предложении. Служит для указания длины массива wordsFromSentence
-            char letterInSentence; //хранит в себе конкретный символ из предложения. Служит для проверки наличия пробела
-            for (int i = 0; i < sentence.Length; i++) //этот алгоритм помогает понять количество слов в предложении. Необходимо для понимания длины массива wordsFromSentence
+            //количество слов в предложении.
+            //Служит для указания длины массива wordsFromSentence
+            int wordsCounter = 1;
+
+            //хранит в себе конкретный символ из предложения.
+            //Служит для проверки наличия пробела
+            char letterInSentence;
+
+            //создаем новый класс StringBuilder для работы с текстом 
+            StringBuilder sb = new StringBuilder();
+
+            //этот алгоритм помогает понять количество слов в предложении.
+            //Необходимо для понимания длины массива wordsFromSentence
+            for (int i = 0; i < sentence.Length; i++) 
             {
-                letterInSentence = sentence[i]; //проверяем по одному символу в предложении
-                if (letterInSentence.ToString() == " ") wordsCounter ++; //если пробел найден, значит в предложение на одно слово больше
+                //проверяем по одному символу в предложении
+                letterInSentence = sentence[i];
+
+                //если пробел найден, значит в предложение на одно слово больше
+                if (letterInSentence.ToString() == " ") wordsCounter ++; 
                 
             }
-            string[] wordsFromSentence = new string[wordsCounter]; //массив, который хранит в себе слова из предложения
-            wordsCounter = 0; //используем данную переменную для записи слов в массив
-            for (int i = 0; i < sentence.Length; i++) //в этом цикле слова из предложения разделяются записываются в массив
+            //массив, который хранит в себе слова из предложения
+            string[] wordsFromSentence = new string[wordsCounter];
+
+            //используем данную переменную для записи слов в массив
+            wordsCounter = 0;
+
+            //в этом цикле слова из предложения разделяются записываются в массив
+            for (int i = 0; i < sentence.Length; i++) 
             {
-                letterInSentence = sentence[i]; //проверяем по одной букве из массива
-                if (letterInSentence.ToString() != " ") //проверка на наличие пробела
+                //проверяем по одной букве из массива
+                letterInSentence = sentence[i];
+                //проверка на наличие пробела
+                if (letterInSentence.ToString() != " ")
                 {
-                    wordsFromSentence[wordsCounter] += letterInSentence; //если символ не является пробелом - добавляем его в текущее слово
+                    //если символ не является пробелом - добавляем его в текущее слово
+                    sb.Append(letterInSentence);
                 }
                 else
                 {
-                    wordsCounter++; //если пробел найден, значит слово закончилось и надо перейти на следующее. Добавляем единицу к счетчику слов
+                    //добавляем текущее слово в массив
+                    wordsFromSentence[wordsCounter] = sb.ToString();
+                    sb.Remove(0, sb.Length);
+                    //если пробел найден, значит слово закончилось и надо
+                    //перейти на следующее. Добавляем единицу к счетчику слов
+                    wordsCounter++;
                 }
+                if (i == sentence.Length - 1) wordsFromSentence[wordsCounter] = sb.ToString();
             }
-            return wordsFromSentence; //возвращаем массив со словами
+            //возвращаем массив со словами
+            return wordsFromSentence;
 
         }
-        static void PrintArray(string[] wordsInArray) //этот метод выводит слова из массива в консоль
+        //этот метод выводит слова из массива в консоль
+        static void PrintArray(string[] wordsInArray) 
         {
             Console.WriteLine("Слова из предложения в массиве:");
             for (int i = 0; i < wordsInArray.Length; i++)
@@ -48,8 +78,10 @@ namespace Methods
         {
             Console.WriteLine("Введите предложение:");
             string sentence = Console.ReadLine();
-            string[] wordsFromSentence = StringToArray(sentence); //вызываем метод, записывающий слова из предложения в массив
-            PrintArray(wordsFromSentence); //вызываем метод, который выводит на экран массив со словами
+            //вызываем метод, записывающий слова из предложения в массив
+            string[] wordsFromSentence = StringToArray(sentence);
+            //вызываем метод, который выводит на экран массив со словами
+            PrintArray(wordsFromSentence); 
             Console.ReadKey();
         }
     }
--- Methods2/Program.cs
+++ Methods2/Program.cs
@@ -8,47 +8,70 @@ namespace Methods2
 {
     internal class Program
     {
-            static void StringToArray(string sentence)
+        //этот метод перемешивает слова в массиве
+            static string ReverseWords(string sentence)
             {
-                int wordsCounter = 1; //количество слов в предложении. Служит для указания длины массива wordsFromSentence
-                char letterInSentence; //хранит в себе конкретный символ из предложения. Служит для проверки наличия пробела
-                for (int i = 0; i < sentence.Length; i++) //этот алгоритм помогает понять количество слов в предложении. Необходимо для понимания длины массива wordsFromSentence
-                {
-                    letterInSentence = sentence[i]; //проверяем по одному символу в предложении
-                    if (letterInSentence.ToString() == " ") wordsCounter++; //если пробел найден, значит в предложение на одно слово больше
-
-                }
-                string[] wordsFromSentence = new string[wordsCounter]; //массив, который хранит в себе слова из предложения
-                wordsCounter = 0; //используем данную переменную для записи слов в массив
-                for (int i = 0; i < sentence.Length; i++) //в этом цикле слова из предложения разделяются записываются в массив
-                {
-                    letterInSentence = sentence[i]; //проверяем по одной букве из массива
-                    if (letterInSentence.ToString() != " ") //проверка на наличие пробела
-                    {
-                        wordsFromSentence[wordsCounter] += letterInSentence; //если символ не является пробелом - добавляем его в текущее слово
-                    }
-                    else
-                    {
-                        wordsCounter++; //если пробел найден, значит слово закончилось и надо перейти на следующее. Добавляем единицу к счетчику слов
-                    }
-                }
-            PrintArray(wordsFromSentence); 
+            //отправляем полученное предложение в метод для разбиения на отдельные слова
+            string[] reversedSentence = SplitText(sentence);
+            Array.Reverse(reversedSentence); //меняет местами слова
+            //соединяет слова из массива в строку
+            sentence = string.Join(" ", reversedSentence);
+            return sentence;
+        }
+        //этот метод разделяет string на отдельные слова в массиве
+        static string[] SplitText(string sentence)
+        {
+            //количество слов в предложении. Служит для указания длины массива wordsFromSentence
+            int wordsCounter = 1;
+            //хранит в себе конкретный символ из предложения. Служит для проверки наличия пробела
+            char letterInSentence;
+            //создаем новый класс StringBuilder для работы с текстом 
+            StringBuilder sb = new StringBuilder();
+            //этот алгоритм помогает понять количество слов в предложении. Необходимо для понимания длины массива wordsFromSentence
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                //проверяем по одному символу в предложении
+                letterInSentence = sentence[i];
+                //если пробел найден, значит в предложение на одно слово больше
+                if (letterInSentence.ToString() == " ") wordsCounter++;
 
             }
-            static void PrintArray(string[] wordsInArray) //этот метод выводит слова из массива в консоль
+            //массив, который хранит в себе слова из предложения
+            string[] wordsFromSentence = new string[wordsCounter];
+            //используем данную переменную для записи слов в массив
+            wordsCounter = 0;
+            //в этом цикле слова из предложения разделяются записываются в массив
+            for (int i = 0; i < sentence.Length; i++)
             {
-                Console.WriteLine("Слова из предложения в массиве:");
-                for (int i = wordsInArray.Length - 1; i >= 0; i--)
+                //проверяем по одной букве из массива
+                letterInSentence = sentence[i];
+                //проверка на наличие пробела
+                if (letterInSentence.ToString() != " ")
                 {
-                    Console.Write($"{wordsInArray[i]}\n");
+                    //если символ не является пробелом - добавляем его в текущее слово
+                    sb.Append(letterInSentence);
                 }
-
+                else
+                {
+                    //добавляем текущее слово в массив
+                    wordsFromSentence[wordsCounter] = sb.ToString();
+                    sb.Remove(0, sb.Length);
+                    //если пробел найден, значит слово закончилось и надо
+                    //перейти на следующее. Добавляем единицу к счетчику слов
+                    wordsCounter++;
+                }
+                if(i == sentence.Length - 1) wordsFromSentence[wordsCounter] = sb.ToString();
             }
+            return wordsFromSentence; //возвращаем массив со словами
+
+        }
             static void Main(string[] args)
             {
                 Console.WriteLine("Введите предложение:");
                 string sentence = Console.ReadLine();
-                StringToArray(sentence); //вызываем метод, записывающий слова из предложения в массив
+            //вызываем метод, меняющий слова местами
+            sentence = ReverseWords(sentence);
+            Console.WriteLine(sentence);
                 Console.ReadKey();
             }
         }
